5e7daf37bc4fe5bd7b022f1e9ebd9c50408031c2
maepocuser2 <maepocuser2@EC2AMAZ-UL0NSS6>
2020-11-03 23:41:47 +0000

    Add project files.

---
+++ Entities/Credit.cs
@@ -0,0 +1,18 @@
+﻿using PaymentService.API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace CreditLibrary.API.Entities
+{
+    public class Credit
+    {
+        public string AccountIdentifier { get; set; }
+        public string SortCode { get; set; }
+        public string PaymentReference { get; set; }
+
+        public double TransferAmount { get; set; }
+
+    }
+}
---
+++ Services/ICreditLibraryRepository.cs
@@ -0,0 +1,15 @@
+﻿using CreditLibrary.API.Entities;
+using PaymentService.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+
+namespace CreditLibrary.API.Services
+{
+    public interface ICreditLibraryRepository
+    {       
+       bool CreditAmount(Credit accountNumber);
+    //    bool DebitAmount(PaymentRequest accountNumber);
+
+    }
+}
